73d7afbca3b22dddb4b2f8a2c09e026af3feae59
kelvinK1ko <88581668+kelvinK1ko@users.noreply.github.com>
2021-08-10 06:23:09 +0100

    Create calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,15 @@
+no1 = int(input("Your first number:"))
+opr = input("Your operation")
+no2 = int(input("Your second number:"))
 
+if opr == "+":
+  print(no1+no2)
+
+if opr == "+":
+  print(no1+no2)
+
+if opr == "+":
+  print(no1+no2)
+
+if opr == "/":
+  print(no1/no2)
